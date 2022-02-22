29baf37fe44fb5264bf2d9df25bb503a34ae2742
Baráth Dávid <davidbarath08@gmail.com>
2022-02-22 08:41:02 +0100

    folytatás

--- Gyepes.java
+++ Gyepes.java
@@ -7,13 +7,28 @@
 // Github: https://github.com/david758/
 // Licenc: GNU GPL
 //
+
+
+//1 négyzetméter-->2500ft
+
 System.out.println("Baráth Dávid,Szoft1/N,2022.02.22");
 
-
+import java.util.Scanner;
 public class Gyepes {
-	
 	public static void main (String[] args) {
-		
+	Scanner sc =new Scanner (System.in);
+	final byte PI=3.14159265359;
+	
+	System.out.print("r");
+	double  = scan.nextDouble();
+	
+	double terulet=Math.pow(r,2)*PI
+	
+	
+	
+	
+	
+	
 	}
 }
 
