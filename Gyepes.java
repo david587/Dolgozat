f3c2584c1cf92ef3e60c48a073a0fb0c0a984a11
Baráth Dávid <davidbarath08@gmail.com>
2022-02-22 08:33:45 +0100

    kezdés

---
+++ Gyepes.java
@@ -0,0 +1,19 @@
+//
+// File: ButtonPanel.java
+//Author: Baráth Dávid
+// Copyright: 2021, Baráth Dávid
+// Group: Szoft 1/N
+// Date: 2022-02-22
+// Github: https://github.com/david758/
+// Licenc: GNU GPL
+//
+System.out.println("Baráth Dávid,Szoft1/N,2022.02.22");
+
+
+public class Gyepes {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
